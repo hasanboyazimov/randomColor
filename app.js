efe4c3fc210df7e7319050464e4771063738491e
Hasanboy <azimovhasanjon1221@gmail.com>
2024-05-22 23:23:59 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const body = document.querySelector("body");
+const container = document.querySelector(".container");
+const colorText = document.querySelector(".color-text");
+
+const values = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+];
+
+// random color funtion
+
+function getGradiant() {
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    const randomNumber = Math.trunc(Math.random() * values.length);
+    color += values[randomNumber];
+  }
+
+  return color;
+}
+
+// set color to body
+function setGradient() {
+  const color1 = getGradiant();
+  const color2 = getGradiant();
+  const randomDeg = Math.trunc(Math.random() * 360);
+  const bgColor = `linear-gradient(
+    ${randomDeg}deg,
+    ${color1}, 
+    ${color2})`;
+
+    body.style.background = bgColor
+    colorText.textContent = bgColor
+}
+
+
+setGradient()
+
+container.addEventListener('click', setGradient)
